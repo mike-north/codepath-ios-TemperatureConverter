65a9b112b0615996f38e6717d1b9e9c2c1552071
Mike North <northm@yahoo-inc.com>
2014-01-12 00:09:24 -0800

    one-way conversion works

---
+++ TemperatureConverter/TemperatureConverterViewController.h
@@ -0,0 +1,13 @@
+//
+//  TemperatureConverterViewController.h
+//  TemperatureConverter
+//
+//  Created by Mike North on 1/11/14.
+//  Copyright (c) 2014 Mike North. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TemperatureConverterViewController : UIViewController
+
+@end
---
+++ TemperatureConverter/TemperatureConverterViewController.m
@@ -0,0 +1,38 @@
+//
+//  TemperatureConverterViewController.m
+//  TemperatureConverter
+//
+//  Created by Mike North on 1/11/14.
+//  Copyright (c) 2014 Mike North. All rights reserved.
+//
+
+#import "TemperatureConverterViewController.h"
+
+@interface TemperatureConverterViewController ()
+
+@end
+
+@implementation TemperatureConverterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
