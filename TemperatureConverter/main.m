ac22769cec3b74809a809136f9a33270055d75de
Mike North <northm@yahoo-inc.com>
2014-01-11 23:46:49 -0800

    Initial Commit

---
+++ TemperatureConverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TemperatureConverter
+//
+//  Created by Mike North on 1/11/14.
+//  Copyright (c) 2014 Mike North. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
